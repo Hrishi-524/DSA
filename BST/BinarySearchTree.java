fcd8c508c6587c6cdbe8be7caef8506cb89ef241
Hrishi-524 <sp3567208@gmail.com>
2025-11-30 16:27:51 +0530

    BST part 2

--- BST/BinarySearchTree.java
+++ BST/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package BST;
 
+import java.util.ArrayList;
+
+import Array.pairs;
 import TreeBinary.BinaryTreesB.Node;
 
 public class BinarySearchTree {
@@ -63,7 +66,13 @@ public class BinarySearchTree {
             }
 
             //case 3 - node with two child
-            Node IS = findInorderSuccessor(root.right);
+            /**
+             *  1. Find IS
+             *  2. Replace to-be-deleted node value with IS value 
+             *  3. Delete IS from Right subtree of to-be-deleted node
+             *  Since recursion is being followed to-be-deleted node = root 
+             */
+            Node IS = findInorderSuccessor(root.right); 
             root.data = IS.data;
             root.right = delete(root.right, IS.data);
 
@@ -87,10 +96,177 @@ public class BinarySearchTree {
         System.out.print(root.data+" ");
         printInorder(root.right);
     }
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if(root == null) {
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        } else if(root.data < k1) {
+            printInRange(root.right, k1, k2);
+        } else {
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    public static boolean leaf(Node root) {
+        return root.left == null && root.right == null;
+    }
+
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if(root == null) {
+            return;
+        }
+
+        path.add(root.data);
+        if(leaf(root)) {
+            System.out.println(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if(root == null) {
+            return true;
+        }
+
+        if(min != null && root.data <= min.data) {
+            return false;
+        }
+
+        if(max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    public static Node mirror(Node root) {
+        //base case
+        if(root == null) {
+            return null;
+        }
+
+        // calculate mirror of left subtree
+        Node leftS = mirror(root.left);
+        // calculate mirror of right subtree
+        Node rightS = mirror(root.right);
+
+        // calculate my own mirror (just swap left and right mirror's)
+        root.left = rightS;
+        root.right = leftS;
+
+        // return tree/subtree
+        return root;
+    }
+
+    public static Node arr2BalancedBST(int[] values, int st, int end) { 
+        if(st >= end) {
+            return null;
+        }
+        
+        int mid = st+(end-st)/2;
+        Node root = new Node(values[mid]);
+
+        root.left = arr2BalancedBST(values, st, mid-1);
+        root.right = arr2BalancedBST(values, mid+1, end);
+
+        return root;
+    }
+
+    public static Node inorder(Node root, ArrayList<Integer> inorder) {
+        if(root == null) {
+            return null;
+        }
+
+        inorder(root.left, inorder);
+        inorder.add(root.data);
+        inorder(root.right, inorder);
+
+        return root;
+    }
+
+    public static Node arr2BalancedBST(ArrayList<Integer> values, int st, int end) { 
+        if(st >= end) {
+            return null;
+        }
+        
+        int mid = st+(end-st)/2;
+        Node root = new Node(values.get(mid));
+
+        root.left = arr2BalancedBST(values, st, mid-1);
+        root.right = arr2BalancedBST(values, mid+1, end);
+
+        return root;
+    }
+
+    public static Node BST2BalancedBST(Node root) { //O(2n) ~ O(n)
+        ArrayList<Integer> inorder = new ArrayList<>();
+
+        root = inorder(root, inorder); // O(n)
+
+        root = arr2BalancedBST(inorder, 0, inorder.size()-1); // O(n)
+
+        return root;
+    }
+
+    private class Info {
+        boolean isBST;
+        int size, min, max;
+        void Info(boolean isBST,int size,int min,int max) {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        } 
+    }
+    
+    public static Info sizeOfLargestBST(Node root, Info info) {
+        if(root == null) {
+            info.isBST = true;
+            info.size = 1;
+            info.min = 0;
+            info.max = 0;
+        }
+        
+        Info leftInfo = sizeOfLargestBST(root.left, info);
+        Info rightInfo = sizeOfLargestBST(root.right, info);
+
+        // 1. deciding if we are valid BST
+        info.isBST = leftInfo.isBST && rightInfo.isBST && isValidBST(root, new Node(Integer.MIN_VALUE), new Node(Integer.MAX_VALUE));
+
+        // 2. calculate own self size
+        info.size = leftInfo.size + rightInfo.size + 1;
+
+        // 3. calculate own min
+        info.min = leftInfo.min;
+
+        // 4. calculate own max
+        info.max = rightInfo.max;
+
+        return info;
+        
+    }
+
     public static void main(String[] args) {
         int[] values = {5, 1, 3, 4, 2, 7};
         Node root = buildBST(values);
-
-        printInorder(root);
+        /**
+         *       5
+         *      / \ 
+         *     1   7
+         *      \     
+         *       3
+         *      / \
+         *     2   4
+         */
+        ArrayList<Integer> path = new ArrayList<>();
+        printRoot2Leaf(root, path);
     }
-}
+}   
